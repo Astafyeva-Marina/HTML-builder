d868443a993c016b551157510170f2eadf1e3787
Alexey Sidorov <astafyeva.marina@mail.ru>
2023-05-09 00:36:52 +0400

    feat: solution 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+const readStream  = fs.createReadStream(path.join(__dirname, "text.txt"), "utf-8");
+
+readStream.on("data", (partData) => console.log(partData));
+readStream.on("error", (error) => console.log(error.message));
